66421a7d58334196fed306cdd3b7c9c5a318eb86
Michael Williamson <63431529+MichaelGW123@users.noreply.github.com>
2021-01-13 19:08:40 -0800

    Update 3080 Webscraping.py

--- Data Collection/3080 Webscraping.py
+++ Data Collection/3080 Webscraping.py
@@ -6,7 +6,6 @@ import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
-#Hello Carrie
 #https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440
 websiteURL = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
 print("Scraping data from " + websiteURL)
